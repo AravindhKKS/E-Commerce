4ccfb4eaada5bd463885631bd186d199554bd1aa
AravindhKKS <94345681+AravindhKKS@users.noreply.github.com>
2024-02-02 13:27:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/product" element={<ProductPage />} />
